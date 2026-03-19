e3cdb5ee071d75c77d50b7e5e5bd4ee29b80cb60
bukxy <peron.nicolas2@gmail.com>
2026-03-19 19:39:04 +0100

    feat: Add messages properties

--- back/src/main/java/com/peron_nicolas/portfolio/ApiApplication.java
+++ back/src/main/java/com/peron_nicolas/portfolio/ApiApplication.java
@@ -1,8 +1,10 @@
 package com.peron_nicolas.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties("messages")
 public class ApiApplication {
 
 	public static void main(String[] args) {
