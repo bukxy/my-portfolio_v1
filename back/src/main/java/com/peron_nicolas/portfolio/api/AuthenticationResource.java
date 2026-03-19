cd5fa9499538e099b293a26416a9abf7d224a238
bukxy <peron.nicolas2@gmail.com>
2026-03-19 15:05:32 +0100

    feat: Use cookie / Add signout route

--- back/src/main/java/com/peron_nicolas/portfolio/api/AuthenticationResource.java
+++ back/src/main/java/com/peron_nicolas/portfolio/api/AuthenticationResource.java
@@ -4,9 +4,10 @@ import com.peron_nicolas.portfolio.entity.User;
 import com.peron_nicolas.portfolio.repository.UserRepository;
 import com.peron_nicolas.portfolio.security.JwtUtil;
 import io.swagger.v3.oas.annotations.Hidden;
-import lombok.NoArgsConstructor;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -30,7 +33,7 @@ public class AuthenticationResource {
     private final JwtUtil jwtUtils;
 
     @PostMapping("/signin")
-    public ResponseEntity<JwtUtil.AuthResponse> authenticateUser(@RequestBody User user) {
+    public ResponseEntity<?> authenticateUser(@RequestBody User user, HttpServletResponse response) {
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         user.getUsername(),
@@ -38,22 +41,46 @@ public class AuthenticationResource {
                 )
         );
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return ResponseEntity.ok(jwtUtils.generateToken(userDetails.getUsername()));
+        JwtUtil.AuthResponse authResponse = jwtUtils.generateToken(userDetails.getUsername());
+
+        ResponseCookie cookie = ResponseCookie.from("accessToken", authResponse.jwtToken())
+                .httpOnly(true)
+                .secure(true)       // false en local, true en prod
+                .sameSite("Strict")
+                .path("/")
+                .maxAge(86400)
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        return ResponseEntity.ok(Map.of("success", true));
     }
 
     // Bloque la création d'un compte utilisateur, car pas besoin :)
-    @PostMapping("/signup")
-    public ResponseEntity<String> registerUser(@RequestBody User user) {
-        if (userRepository.existsByUsername(user.getUsername())) {
-            return ResponseEntity.badRequest().build();
-        }
+//    @PostMapping("/signup")
+//    public ResponseEntity<String> registerUser(@RequestBody User user) {
+//        if (userRepository.existsByUsername(user.getUsername())) {
+//            return ResponseEntity.badRequest().build();
+//        }
+//
+//        final User newUser = new User(
+//                null,
+//                user.getUsername(),
+//                encoder.encode(user.getPassword())
+//        );
+//        userRepository.save(newUser);
+//        return ResponseEntity.ok("User registered successfully!");
+//    }
 
-        final User newUser = new User(
-                null,
-                user.getUsername(),
-                encoder.encode(user.getPassword())
-        );
-        userRepository.save(newUser);
-        return ResponseEntity.ok("User registered successfully!");
+    @PostMapping("/signout")
+    public ResponseEntity<?> signout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("accessToken", "")
+                .httpOnly(true)
+                .path("/")
+                .maxAge(0)  // expire immédiatement
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        return ResponseEntity.ok(Map.of("success", true));
     }
 }
