58700c54312aa4d038b994dbc8f56e28e3c98ee3
bukxy <peron.nicolas2@gmail.com>
2026-03-20 17:24:30 +0100

    feat: Add Requester service

---
+++ front/src/app/services/requester/requester-service.ts
@@ -0,0 +1,28 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequesterService {
+
+  private httpClient = inject(HttpClient);
+
+  get<T>(url: string, params?: any): Observable<T> {
+    return this.httpClient.get<T>(environment.apiURL + url, {params});
+  }
+
+  post<T>(url: string, params?: any, withCredentials = false): Observable<T> {
+    return this.httpClient.post<T>(environment.apiURL + url, params, {withCredentials});
+  }
+
+  put<T>(url: string, params?: any): Observable<T> {
+    return this.httpClient.put<T>(environment.apiURL + url, params);
+  }
+
+  delete(url: string, params?: any): Observable<Object> {
+    return this.httpClient.delete(environment.apiURL + url, params);
+  }
+}
