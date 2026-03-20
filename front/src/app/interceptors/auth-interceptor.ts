10d4e72cc345b2312201d55be185f6c43a8de296
bukxy <peron.nicolas2@gmail.com>
2026-03-20 17:22:04 +0100

    feat: Add AuthInterceptor

---
+++ front/src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,37 @@
+// auth.interceptor.ts
+import {HttpInterceptorFn, HttpRequest, HttpHandlerFn, HttpClient} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap, throwError } from 'rxjs';
+import {AuthService} from '../services/jwt-token/auth-service';
+
+export const AuthInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  const authService = inject(AuthService);
+  const token = authService.getAccessToken();
+
+  const authReq = token
+    ? req.clone({ headers: req.headers.set('Authorization', `Bearer ${token}`) })
+    : req;
+
+  return next(authReq).pipe(
+    catchError(err => {
+      if (err.status === 401) {
+        const http = inject(HttpClient);
+        return http.post<{ access_token: string }>('/api/v1/auth/refresh', null,
+          { withCredentials: true }
+        ).pipe(
+          switchMap(res => {
+            authService.setAccessToken(res.access_token);
+            return next(authReq.clone({
+              headers: authReq.headers.set('Authorization', `Bearer ${res.access_token}`)
+            }));
+          }),
+          catchError(() => {
+            authService.removeAccessToken();
+            return throwError(() => err);
+          })
+        );
+      }
+      return throwError(() => err);
+    })
+  );
+};
