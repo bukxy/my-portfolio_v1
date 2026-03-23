70a2cd7f95df2873e387a811158e3cc6bbbd3fa3
bukxy <peron.nicolas2@gmail.com>
2026-03-23 20:48:58 +0100

    feat: Skill entity

---
+++ back/src/main/java/com/peron_nicolas/portfolio/entity/Skill.java
@@ -0,0 +1,32 @@
+package com.peron_nicolas.portfolio.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Skill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Byte percentage;
+
+    public Skill(String name, Byte percentage) {
+        this.name = name;
+        this.percentage = percentage;
+    }
+
+    public Skill() {
+        
+    }
+}
