551413e5c5d1b6c679028668c2deda1d1bf22e93
bukxy <peron.nicolas2@gmail.com>
2026-03-23 20:49:03 +0100

    feat: Skill DTO

---
+++ back/src/main/java/com/peron_nicolas/portfolio/dto/skill/SkillDTO.java
@@ -0,0 +1,16 @@
+package com.peron_nicolas.portfolio.dto.skill;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record SkillDTO(
+    @NotBlank(message = "Name is required")
+    String name,
+
+    @NotNull(message = "Percentage is required")
+    @Min(value = 0, message = "Percentage must be between 0 and 100")
+    @Max(value = 100, message = "Percentage must be between 0 and 100")
+    Byte percentage
+) {}
