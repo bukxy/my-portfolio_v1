9bf876e6203aa40c1c1e3dbdf574ef4d6f42fd4b
bukxy <peron.nicolas2@gmail.com>
2026-03-23 20:42:51 +0100

    feat: Custom response pattern

--- back/src/main/java/com/peron_nicolas/portfolio/tools/MessageTool.java
+++ back/src/main/java/com/peron_nicolas/portfolio/tools/MessageTool.java
@@ -3,7 +3,11 @@ package com.peron_nicolas.portfolio.tools;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +17,12 @@ public class MessageTool {
 
     public String set(String messageKey, Object... args) {
         return messageSource.getMessage(messageKey, args, LocaleContextHolder.getLocale());
+    }
+
+    public ResponseEntity<?> res(HttpStatus HttpStatusCode, Object data, String messageKey, Object... args) {
+        return ResponseEntity.status(HttpStatusCode).body(Map.of(
+                "message", messageSource.getMessage(messageKey, args, LocaleContextHolder.getLocale()),
+                "data", data
+        ));
     }
 }
