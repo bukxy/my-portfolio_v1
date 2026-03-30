6f1d13ef7111c7d34aa792659343d68c7c58a496
bukxy <peron.nicolas2@gmail.com>
2026-03-30 21:45:49 +0200

    feat: Return image url

--- back/src/main/java/com/peron_nicolas/portfolio/dto/image/ImageMapper.java
+++ back/src/main/java/com/peron_nicolas/portfolio/dto/image/ImageMapper.java
@@ -1,6 +1,7 @@
 package com.peron_nicolas.portfolio.dto.image;
 
 import com.peron_nicolas.portfolio.entity.Image;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -8,8 +9,11 @@ import java.util.List;
 @Component
 public class ImageMapper {
 
+    @Value("${back.url}")
+    private String backUrl;
+
     public ImageDTO toDto(Image image) {
-        return new ImageDTO(image.getId(), image.getPath());
+        return new ImageDTO(image.getId(), backUrl + image.getPath());
     }
 
     public List<ImageDTO> toDto(List<Image> images) {
