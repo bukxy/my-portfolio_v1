08f7661ac336f33b78079ab14af7a9b02422ef9f
bukxy <peron.nicolas2@gmail.com>
2026-03-30 21:40:57 +0200

    feat: Add markdown

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -6,12 +6,14 @@ import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {AuthInterceptor} from './interceptors/auth-interceptor';
 import {Overlay} from '@angular/cdk/overlay';
 import {MAT_DIALOG_DEFAULT_OPTIONS} from '@angular/material/dialog';
+import {provideMarkdown} from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideMarkdown(),
     // provideZoneChangeDetection()
     {
       provide: MAT_DIALOG_DEFAULT_OPTIONS,
