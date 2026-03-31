08d334765834518699a094c20069d21d53ca2abb
bukxy <peron.nicolas2@gmail.com>
2026-03-31 17:58:21 +0200

    feat: Add translate menu

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,15 +1,19 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {ThemeToggleComponent} from '../theme-toggle/theme-toggle.component';
 import {logotext, socialprofils} from '../../content_option';
 import {Login} from '../../dialogs/login/login';
 import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatTooltip} from '@angular/material/tooltip';
+import {LangService} from '../../services/lang/lang-service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, ThemeToggleComponent, RouterLinkActive, Login, MatIcon],
+  imports: [RouterLink, ThemeToggleComponent, RouterLinkActive, Login, MatIcon, MatIconButton, MatMenu, MatMenuTrigger, MatTooltip, MatMenuItem],
   templateUrl: './header.component.html',
   styles: [`
     button {
@@ -22,6 +26,8 @@ import {MatIcon} from '@angular/material/icon';
   `]
 })
 export class HeaderComponent {
+
+  langService = inject(LangService);
 
    navItems: [string, string][] = [
     ["/", "Home"],
